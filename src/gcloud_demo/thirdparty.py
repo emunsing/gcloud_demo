4fbd204136cfcaec69b21f85b139b23f74832ce0
emunsing <eric.munsing@gmail.com>
2025-05-09 14:24:19 -0700

    Simplify geocoding to just zip code

--- src/gcloud_demo/thirdparty.py
+++ src/gcloud_demo/thirdparty.py
@@ -4,54 +4,20 @@ import requests
 import logging
 
 GMAPS_API_KEY = os.getenv('GOOGLE_API_KEY')
-ADDRESS_VALIDATION_URL = f'https://addressvalidation.googleapis.com/v1:validateAddress?key={GMAPS_API_KEY}'
+GEOCODING_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
-@attr.s(auto_attribs=True, kw_only=True)
-class Address:
-    street_address: str = '' # Street address, e.g. "111 Main Street"
-    city: str = '' #
-    state: str = '' # state
-    postal_code: str = '' # Zip code
-    country: str = 'US'
-    latitude: float = None # Latitude
-    longitude: float = None
-
-def validate_address(addr: Address) -> [Address, None]:
-    """
-    Accepts an address as a dictionary, returns updated address, latitude, and longitude.
-    Adapted from https://github.com/emunsing/turfcutter
-    """
-    # Structure the address for the API request
-    address = {
-        "address": {
-            "regionCode": addr.country,
-            "postalCode": addr.postal_code,
-            "administrativeArea": addr.state,
-            "locality": addr.city,
-            "addressLines": [addr.street_address]
-        }
+# Function to get latitude and longitude from an address
+def get_lat_long(address):
+    params = {
+        'address': address,
+        'key': GMAPS_API_KEY
     }
-
-    # Send request to Address Validation API
-    response = requests.post(ADDRESS_VALIDATION_URL, json=address)
+    response = requests.get(GEOCODING_API_URL, params=params)
     result = response.json()
 
-    if 'result' in result and 'geocode' in result['result']:
-        validated_address = result['result']['address']
-        try:
-            corrected_address = Address(
-                street_address=validated_address['postalAddress']['addressLines'][0],
-                city=validated_address['postalAddress']['locality'],
-                state=validated_address['postalAddress']['administrativeArea'],
-                postal_code=validated_address['postalAddress']['postalCode'],
-                country=validated_address['postalAddress']['regionCode'],
-                latitude=result['result']['geocode']['location']['latitude'],
-                longitude=result['result']['geocode']['location']['longitude']
-            )
-            return corrected_address
-        except KeyError as e:
-            logging.warning(f"Key error: {e}")
-            return None
+    if result['status'] == 'OK':
+        location = result['results'][0]['geometry']['location']
+        return location['lat'], location['lng']
     else:
-        logging.warning("Error:", result.get("error", "Unknown error"))
-        return None
+        print(f"Error geocoding {address}: {result['status']}")
+        return None, None
--- src/gcloud_demo/thirdparty_populator.py
+++ src/gcloud_demo/thirdparty_populator.py
@@ -5,11 +5,6 @@ import logging
 
 CA_ZIP_RANGE = (90001, 96162)
 
-# A set of randomly generated but vetted zip codes which contain main street
-ZIPCODES_WITH_MAIN_STREET = ["90242", "96054", "91361", "91566", "91880", "93723", "92545",
-                             "92064", "90003", "94042", "92119", "90061", "92806", "93319",
-                             "92410", "92990", "90519", "92793", "93105", "92662", "91244",
-                             "93649", "91364" ]
 
 @click.group()
 def cli():
@@ -17,27 +12,15 @@ def cli():
 
 
 @cli.command()
-@click.option('--n', default=100, help='Number of random addresses to generate.')
-@click.option('--random-zip', is_flag=True, help='Generate random addresses.')
-def request_ramdom_addresses(n, random_zip):
+@click.option('--n', default=10, help='Number of random addresses to generate.')
+def request_ramdom_addresses(n):
     """
     Request random addresses from the third-party API.
     """
     for i in range(n):
-        if random_zip:
-            zip_code = np.random.randint(*CA_ZIP_RANGE)
-        else:
-            zip_code = np.random.choice(ZIPCODES_WITH_MAIN_STREET)
-        address_request = thirdparty.Address(postal_code=str(zip_code),
-                                             street_address=f"{np.random.randint(1, 100)} Main St"
-                                             )
-        updated_address = thirdparty.validate_address(address_request)
-        if updated_address is None:
-            continue
-        if address_request.postal_code not in updated_address.postal_code:
-            logging.warning(f"Street address does not exist in zip {address_request.postal_code}")
-        else:
-            logging.warning(f"{updated_address.street_address}, {updated_address.city} {updated_address.postal_code}: {updated_address.latitude}, {updated_address.longitude}")
+        zipcode = np.random.randint(*CA_ZIP_RANGE)
+        lat, lon = thirdparty.get_lat_long(zipcode)
+        logging.warning(f"Zipcode: {zipcode}, Lat: {lat}, Lon: {lon}")
 
 if __name__ == '__main__':
     cli()
